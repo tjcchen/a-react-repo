a3d25044c4cf945ba64d4817ad0c0d0299be9ca5
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-22 21:25:22 +0800

    feat: adding multiple same code block

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,26 @@
 function App() {
   return (
-    <div>
-      <img
-        style={{ width: '16rem' }}
-        src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
-        alt="animal"
-      />
-      <p>Beau chat</p>
-      <a href="/cats">More infomation</a>
-    </div>
+    <>
+      <div>
+        <img
+          style={{ width: "16rem" }}
+          src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
+          alt="animal"
+        />
+        <p>Beau chat</p>
+        <a href="/cats">More infomation</a>
+      </div>
+
+      <div>
+        <img
+          style={{ width: "16rem" }}
+          src="https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
+          alt="animal"
+        />
+        <p>Beau chat</p>
+        <a href="/cats">More infomation</a>
+      </div>
+    </>
   );
 }
 
