5568d5b1ec99134b525d7cb97d0d3911f379ab9a
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-24 23:14:58 +0800

    feat: adding a counter example

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,11 +1,15 @@
 import Card from "./components/Card/card";
 import Event from "./components/Card/event";
+import Counter from "./components/Card/state";
 
 function App() {
   return (
     <>
       <Card />
+      <hr/>
       <Event />
+      <hr/>
+      <Counter />
     </>
   );
 }
--- react-app/src/components/Card/state.jsx
+++ react-app/src/components/Card/state.jsx
@@ -4,7 +4,20 @@
  * - useState Hook
  *   Hook = functions that 'hooks into' your component
  * - Using the React Developer Tools to check state
- * 
+ *
  * - Counter & Toggle
  *   prevState
  */
+
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>Increment Counter</button>
+      <p>{count}</p>
+    </>
+  );
+}
