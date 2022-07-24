127eb7fcfe12de08a5e862d8fff6cd4a412c9975
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-24 23:07:35 +0800

    docs: comment thing for basic state

---
+++ react-app/src/components/Card/state.jsx
@@ -0,0 +1,10 @@
+/**
+ * Basic State
+ * - State = data stored in an app at a specific time
+ * - useState Hook
+ *   Hook = functions that 'hooks into' your component
+ * - Using the React Developer Tools to check state
+ * 
+ * - Counter & Toggle
+ *   prevState
+ */
