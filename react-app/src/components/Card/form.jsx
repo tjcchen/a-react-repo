7b032507189376e3b52996b03564d3a0b107bf39
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-28 00:05:09 +0800

    feat: combine form data with useState

--- react-app/src/components/Card/form.jsx
+++ react-app/src/components/Card/form.jsx
@@ -7,18 +7,32 @@
  *   e.preventDefault
  */
 
+import { useState } from "react";
+
 export default function Form() {
+  const [formData, setFormData] = useState({
+    title: "",
+    body: "",
+  });
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <h1>Form</h1>
       <p>Create a post</p>
       <form action="">
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" />
+        <input type="text" id="title" onChange={onChange} />
         <br />
         <br />
         <label htmlFor="body">Body</label>
-        <input type="text" id="body" />
+        <input type="text" id="body" onChange={onChange} />
         <br />
         <br />
         <button type="submit">Upload post</button>
