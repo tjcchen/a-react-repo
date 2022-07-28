e2fb40338c375dc334543d6b073875f94ba51a0e
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-28 23:39:18 +0800

    feat: adding on submit event

--- react-app/src/components/Card/form.jsx
+++ react-app/src/components/Card/form.jsx
@@ -9,11 +9,14 @@
 
 import { useState } from "react";
 
+const defaultFormData = {
+  title: "",
+  body: "",
+};
+
 export default function Form() {
-  const [formData, setFormData] = useState({
-    title: "",
-    body: "",
-  });
+  const [formData, setFormData] = useState(defaultFormData);
+  const { title, body } = formData;
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -22,17 +25,23 @@ export default function Form() {
     }));
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData(defaultFormData);
+  };
+
   return (
     <>
       <h1>Form</h1>
       <p>Create a post</p>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" onChange={onChange} />
+        <input type="text" id="title" value={title} onChange={onChange} />
         <br />
         <br />
         <label htmlFor="body">Body</label>
-        <input type="text" id="body" onChange={onChange} />
+        <input type="text" id="body" value={body} onChange={onChange} />
         <br />
         <br />
         <button type="submit">Upload post</button>
