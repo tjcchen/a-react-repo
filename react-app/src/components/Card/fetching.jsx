a6154d7f657aa61787f5643f55be2fa34c3da3f9
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-29 20:35:37 +0800

    feat: adding docs for data fetching

---
+++ react-app/src/components/Card/fetching.jsx
@@ -0,0 +1,13 @@
+/**
+ * Data Fetching - Get Data
+ * - Using Axios
+ * - GET
+ * - useEffect
+ * - key attribute( when use .map )
+ * - loading state
+ * - error state
+ * 
+ * Info
+ * - React Query( https://github.com/TanStack/query )
+ * - Axios( https://github.com/axios/axios )
+ */
