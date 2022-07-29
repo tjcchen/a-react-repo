a76ecbef8ed6a31ac400e8ba97f2f564e78365da
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-29 23:36:59 +0800

    feat: adding a loading state

--- react-app/src/components/Card/fetching.jsx
+++ react-app/src/components/Card/fetching.jsx
@@ -18,13 +18,18 @@ import { useEffect, useState } from "react";
 
 export default function Fetching() {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
+      setLoading(true);
+
       const response = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
+
       setData(response.data);
+      setLoading(false);
     };
 
     fetchPosts();
@@ -33,6 +38,7 @@ export default function Fetching() {
   return (
     <>
       <p>Fetch GET Request</p>
+      <p>{loading && "Loading..."}</p>
       {data &&
         data.map((post) => {
           const { id, title, body } = post;
