0366605f5a5c92b14f4c1464ce504cdc18665605
Andy Chen <chenyangpoppin@hotmail.com>
2022-07-30 22:07:01 +0800

    feat: changing entry point file

---
+++ react-app/src/App3.jsx
@@ -0,0 +1,15 @@
+/**
+ * Entry of App3
+ */
+
+import Form from "./components/Card/form";
+
+ function App() {
+   return (
+     <>
+       <Form />
+     </>
+   );
+ }
+ 
+ export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import App from './App2';
+// import App from './App2';
+import App from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
