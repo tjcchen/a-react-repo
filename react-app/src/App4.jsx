90702dc7055f0492276449f7301f95f7333263ae
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-01 23:09:07 +0800

    docs: docs concerning React Router Hooks

---
+++ react-app/src/App4.jsx
@@ -0,0 +1,18 @@
+/**
+ * React Router Hooks
+ * - useHistory
+ *   history.push()
+ *   with and without state
+ *   vs <link />
+ * - history.goBack()
+ * - useParams
+ *   Destructure from URL
+ */
+
+export default function App() {
+  return (
+    <>
+      <p>React Router Hooks</p>
+    </>
+  );
+}
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
 // import App from './App2';
-import App from './App3';
+// import App from './App3';
+import App from './App4';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
