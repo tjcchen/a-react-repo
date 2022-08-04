1ecd4318c79c5ffbebf677d1054465ad0fd7461c
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-04 23:17:14 +0800

    feat: passing data from component A to component B

--- react-app/src/router/Dashboard.jsx
+++ react-app/src/router/Dashboard.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
   const navigate = useNavigate();
+  const data = 54897;
 
   return (
     <>
       <p>Dashboard</p>
-      <button onClick={() => navigate("/login", { replace: false })}>
+      <button onClick={() => navigate("/login", { replace: false, state: data })}>
         Go to login
       </button>
     </>
--- react-app/src/router/Login.jsx
+++ react-app/src/router/Login.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location);
+
   return (
     <>
       <p>Login</p>
+      <p>{location.state}</p>
     </>
   );
 }
