69a113f3284c1b91663f7d3939dcaac12463e149
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-10 23:02:44 +0800

    feat: configure code snippet for ContextAPI

---
+++ react-app/src/context/InputValueContext.js
@@ -0,0 +1,42 @@
+/**
+ * Snippet Generator
+ * link: https://snippet-generator.app/
+ * how to config: code -> preference -> code snippets -> javasript react
+ */
+
+import React, { createContext, useReducer } from "react";
+
+const initialState = {
+  inputValue: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_INPUT_VALUE":
+      return {
+        ...state,
+        inputValue: action.payload,
+      };
+    case "SET_INPUT_VALUE_TO_100":
+      return {
+        ...state,
+        inputValue: 100,
+      };
+    default:
+      return state;
+  }
+};
+
+const InputValueContext = createContext({ state: initialState, dispatch: () => {} });
+
+function InputValueProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <InputValueContext.Provider value={{ state, dispatch }}>
+      {children}
+    </InputValueContext.Provider>
+  );
+}
+
+export { InputValueContext, InputValueProvider };
