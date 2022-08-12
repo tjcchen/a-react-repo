da61268f91edf62beebd9eb4ec668e2a1da0ec18
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-12 21:05:28 +0800

    chore: adding dependencies

--- react-app/src/App7.jsx
+++ react-app/src/App7.jsx
@@ -1,6 +1,13 @@
 /**
  * React Query Intro
  * - Why use React-Query
+ * 1) Library focused on making data-fetching simple and efficient
+ *   * Easy to understand for other developers
+ *   * Build in tools to save bandwidth
+ * 2) Caching => Great for the UX ( faster & responsive )
+ * 3) Customizable
+ *   retries, paginated queries, prefetching, optimistic updates etc.
+ * 
  * - Architecture
  * - install
  *   `npm install react-query`
