d975da39d3ef60ec918fb7c43c33b8bdda8f21da
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-17 22:53:20 +0800

    docs: comment thing

--- react-app/src/App8.jsx
+++ react-app/src/App8.jsx
@@ -1,3 +1,16 @@
+/**
+ * Using Redux
+ * - It's recommended to use Redux Toolkit
+ *  `npm install @reduxjs/toolkit react-redux`
+ * 
+ * - Create a store
+ * - Provide the store to the App Component
+ * - Create a Redux State Slice
+ * - Add Slice Reducers to the Store
+ * - Use React-Redux hooks to get/set data
+ *   Get: useSelector()
+ *   Set: useDispatch()
+ */
 import { useState } from "react";
 import CalculationOverview from "./state/CalculationOverview3";
 import InputNumber from "./state/InputNumber";
