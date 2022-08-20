e134015a65fd36fb24bbb524bc7b00a534f71e7f
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-20 21:53:33 +0800

    chore: comment thing

--- react-app/src/App9.jsx
+++ react-app/src/App9.jsx
@@ -1,13 +1,30 @@
 /**
  * Styling Intro
  * - Styling options
- * 1) CSS/SCSS
+ * 1) CSS/SCSS ( Specificity Problems )
  * 2) CSS Modules
  * 3) Styled Components / Emotion
  * 
  * Other:
  * - Component Libraries( Eg: Material UI, Ant Design )
  * - CSS Frameworks( Eg: Tailwind, Sematic UI )
+ * 
+ * ============================================
+ * CSS Modules vs Styled Components
+ * CSS Modules
+ * - Pros
+ * 1) No-learning curve
+ * 2) Comes with CRA
+ * 
+ * Styled Components
+ * - Pros
+ * 1) No unused CSS
+ * 2) Easier deletion / copy-paste of styles
+ * 3) Allows for snapshot testing your styles
+ * 
+ * - Cons
+ * 1) HTML / Dom tree becomes hard to read( inline styles )
+ * 2) Code completion is not so good
  */
 
 export default function App() {
