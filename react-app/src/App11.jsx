9590532bcfb2a42d716327b6478292930bfc32a6
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-25 23:27:24 +0800

    chore: update links

--- react-app/src/App11.jsx
+++ react-app/src/App11.jsx
@@ -1,9 +1,9 @@
 /**
  * Performance Intro
  * - Focus on (potential) react-specific performance issues / improvements
- * - Don't forget tools like lighthouse (web.dev), GTmetrix, Pingdom etc.
+ * - Don't forget tools like lighthouse (web.dev[important]), GTmetrix(https://gtmetrix.com/), Pingdom etc.
  * - Besides this secions about performance, we will also be taking a look at
- *   Web Vitals in the Monitoring section of this course
+ *   Web Vitals(https://web.dev/vitals/) in the Monitoring section of this course
  */
 export default function App() {
   return (
