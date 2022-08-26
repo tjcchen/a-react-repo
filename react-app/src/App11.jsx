241edf5325b04946e7d77e25fc1546e401f59b8e
Andy Chen <chenyangpoppin@hotmail.com>
2022-08-26 22:01:04 +0800

    docs: rendering explanation

--- react-app/src/App11.jsx
+++ react-app/src/App11.jsx
@@ -4,6 +4,13 @@
  * - Don't forget tools like lighthouse (web.dev[important]), GTmetrix(https://gtmetrix.com/), Pingdom etc.
  * - Besides this secions about performance, we will also be taking a look at
  *   Web Vitals(https://web.dev/vitals/) in the Monitoring section of this course
+ * 
+ * Rendering:
+ * If we cause a component to (re)render, React works in 2 phases:
+ * - Render: determining what changes need to be made (this is where the reconciliation / 'diff' algorithm comes in to play)
+ * - Commit: apply the changes (e.g. inserting / updating / removing DOM nodes)
+ * 
+ * Commiting = Fast, Rendering = can be slow
  */
 export default function App() {
   return (
