c11b1a699e8bab89ca4144cf539c4721a997b199
Andy Chen <chenyangpoppin@hotmail.com>
2022-09-14 23:32:27 +0800

    docs: comment thing

--- memo/src/components/Memo.jsx
+++ memo/src/components/Memo.jsx
@@ -1,3 +1,7 @@
+// Both React.useMemo and React.useCallback receives a function as its first argument and a dependencies array as the second one.
+// The hook will return a new value only when one of the dependencies value changes (referential equality).
+// The main difference is that React.useMemo will call the getStr and return its result
+// while React.useCallback will return the getStr without calling it.
 import React, { useCallback, useMemo } from "react";
 
 export default function Memo() {
@@ -5,6 +9,10 @@ export default function Memo() {
     return 'useMemo & useCallback hooks';
   };
 
+  // useMemo returns a memoized value
+  // useCallback returns a memoized callback
+  // useMemo call the function and return its result
+  // useCall return the function without calling it
   const useMemoResult = useMemo(getStr, []);
   const useCallbackResult = useCallback(getStr, []);
 
